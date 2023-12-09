19892fa12a5efe949930052ded6c2e63d588f07b
nayrasleao <nayraleao.souza@gmail.com>
2023-12-08 22:00:51 -0400

    feat(html/css/js): add image div, apply CSS styles, and implement JavaScript functionality
    Adds a new div for the image in the HTML, includes corresponding CSS styles, and implements JavaScript functionality for rotating the logo.

--- src/js/about.js
+++ src/js/about.js
@@ -1,4 +1,14 @@
+// Função para ativar menu hamburguer
 document.getElementById("navToggle").addEventListener("click", function () {
   var nav = document.getElementById("nav");
   nav.classList.toggle("active", !nav.classList.contains("active"));
 });
+
+// Função para girar a logo
+function rotateLogo() {
+  const logo = document.getElementById('logo');
+  logo.style.transform = `rotate(${(360 * Math.random())}deg)`;
+}
+
+// Inicia a animação a cada 3 segundos (ajuste conforme necessário)
+setInterval(rotateLogo, 300);
